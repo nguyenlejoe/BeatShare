f98fa32dc29058377e48a823a6af2bad635fb9e6
nguyenlejoe <joe_le.1@hotmail.com>
2021-02-24 15:09:19 -0800

    set up routing and pages for development phase

---
+++ src/comps/NavBar/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const NavCont = styled.div`
+
+`
+
+const NavBar = () => {
+    return (
+        <NavCont>
+            
+        </NavCont>
+    );
+}
+
+defaultProps.NavBar = {
+
+}
+
+export default NavBar;
---
+++ src/pages/HomePage/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className="Home">
+            BeatShare
+        </div>
+    );
+}
+
+export default Home;
