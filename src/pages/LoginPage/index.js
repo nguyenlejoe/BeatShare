6087c7a19633ba44b6061567a2e42dd1972f1e9b
Your Name <you@example.com>
2021-03-04 14:05:36 -0800

    login setup

---
+++ src/pages/LoginPage/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import "login.scss"
+
+
+const Login = () => {
+    return (
+        <div className="Main">
+            <h1>BeatShare</h1>
+
+
+      </div>
+
+    );
+}
+
+export default Login;
