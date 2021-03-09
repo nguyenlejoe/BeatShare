c070643abf64b403d638a329731c4e7671e7c3e1
Your Name <you@example.com>
2021-03-08 19:59:57 -0800

    profile page setup + input styling

--- src/pages/AccountPage/index.js
+++ src/pages/AccountPage/index.js
@@ -1,37 +1,18 @@
 import React from 'react';
 import NavBar from "../../comps/NavBar";
-import PostBtn from "../../comps/PostBtn";
-import PFP from "../../comps/Pfp"
-import Button from "../../comps/Button"
-import MenuBtn from "../../images/menuBtn.svg"
-import BackBtn from "../../images/backBtn.svg"
-import Logo from "../../images/BeatShareLogo.svg"
 import ProfileTop from "../../comps/ProfileTop"
+import MenuBtn from '../../images/menuBtn.svg';
 
 import '../../App.scss';
 
 const Account = () => {
     return (
         <div className="Main">
-
-            {/* Kai testing his comps  */}
             
-            <br></br>
-            <PostBtn />
-            <br></br>
-            <PFP />
-            <br></br>
-            <Button text="Log in"/>
-            <br></br>
-            <Button text="Sign up"/>
-            <br></br>
-            <img src={MenuBtn} />
-            <br></br>
-            <img src={BackBtn} />
-            <br></br>
-            <img src={Logo} />
-
-            {/* end of kai testing his comps */}
+            <img src={MenuBtn} className="menu"/>
+            
+            <ProfileTop />
+            
             <div className="Nav">
                 <NavBar />
             </div>
