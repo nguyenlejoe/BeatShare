2a235ab857fad54954cceb665f882a1272ac4633
Your Name <you@example.com>
2021-03-08 20:58:45 -0800

    profile menu state

---
+++ src/comps/ProfileMenu/index.js
@@ -0,0 +1,58 @@
+import react from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+display:flex;
+flex-direction:column;
+padding:5px;
+height:185px;
+width:165px;
+z-index:1;
+background-color:#171717;
+border-radius:7px;
+color:white;
+`;
+
+const Option = styled.div`
+padding:10px;
+&:hover{
+    background-color:#7209B7;
+    transition: background-color, 0.5s;
+}
+`;
+
+const Line = styled.div`
+width: 160px;
+height: 1px;
+background-color: #282828;
+`;
+
+
+
+const ProfileMenu = ({onClick }) => {
+    
+
+    return (
+        <Container>
+            <Option onClick={onClick}>
+                Log out
+            </Option>
+            <Line/>
+            <Option onClick={onClick}>
+                Edit Profile
+            </Option>
+            <Line/>
+            <Option onClick={onClick}>
+                View Likes
+            </Option>
+            <Line/>
+            <Option onClick={onClick}>
+                Settings
+            </Option>
+            <Line/>
+        </Container>
+    );
+}
+
+
+export default ProfileMenu;
--- src/pages/AccountPage/index.js
+++ src/pages/AccountPage/index.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import NavBar from "../../comps/NavBar";
 import ProfileTop from "../../comps/ProfileTop"
 import MenuBtn from '../../images/menuBtn.svg';
+import ProfileMenu from '../../comps/ProfileMenu';
 
 import '../../App.scss';
 
 const Account = () => {
+    const [show, setShow] = useState(true);
+
+
     return (
         <div className="Main">
-            
-            <img src={MenuBtn} className="menu"/>
+            <img src={MenuBtn} className="menubtn" onClick={()=> setShow(!show)}/>
+
+            {show ? (
+                            <div className='profile_menu'>
+            <ProfileMenu />  
+            </div>
+            )          
+            : null
+        }
+
+
             
             <ProfileTop />
             
